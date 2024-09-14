0647f56050fc8c4c1055075486a503dd922fe076
Ganzadidier <d.ganza@alustudent.com>
2024-09-14 18:23:13 +0000

    Removing white spaces

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,16 +4,16 @@
 
 def minOperations(n):
     """Minimum ops"""
-    if n<=1:
+    if n <= 1:
         return 0
 
-    i=2
-    operation=0
+    i = 2
+    operation = 0
 
     while n > 1:
         while n % i == 0:
             operation += i
             n //= i
         i += 1
-    
+
     return operation
