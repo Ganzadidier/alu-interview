b79252d0eab1c457d60cfd2ca57d6338f273c5c2
Ganzadidier <d.ganza@alustudent.com>
2024-09-28 14:25:03 +0000

    pcodestyle issues

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,26 +5,26 @@
 def rain(walls):
     """
     This function calculates the total amount of rainwater.
-    
+
     Parameters:
     walls (list): A list of non-negative integers
-    
+
     Returns:
-    int: The total number of units of water that can be trapped between the walls.
+    int: The total number of units of water that can be trapped btn the walls
     """
     # If the list is empty, no water can be trapped, so return 0
     if not walls:
         return 0
-    
+
     # Initialize two pointers, from the left and the other from the right
     left, right = 0, len(walls) - 1
-    
+
     # Initialize variables to keep track of the maximum wall heights
     left_max, right_max = walls[left], walls[right]
-    
+
     # Variable to accumulate the total amount of water trapped
     water_trapped = 0
-    
+
     # Loop until the two pointers meet
     while left < right:
         # If the wall on the left is shorter or equal to the one on the right
@@ -34,7 +34,7 @@ def rain(walls):
                 # Water trapped is the difference between left_max
                 water_trapped += left_max - walls[left]
             else:
-                # Update left_max if the current wall is the tallest seen to the left
+                # Update left_max if the current wall is the tallest to left
                 left_max = walls[left]
             # Move the left pointer to the right
             left += 1
@@ -48,6 +48,6 @@ def rain(walls):
                 right_max = walls[right]
             # Move the right pointer to the left
             right -= 1
-    
+
     # Return the total amount of water trapped
     return water_trapped
